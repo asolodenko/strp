242d031c46af58032a5b5aee3aada2d18fe37d17
aryna.solodenko <aryna.karali@itera.com>
2023-04-18 19:35:20 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '06bcc55cf185630e1a323152e3314f83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e95cf181-fb94-4765-9694-e7515baa9cea'
+};
